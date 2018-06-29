a8efef3711e06d0b906188124c85d4aa2a3fc20f
qiandajun <qiandajun@appledeMacBook-Pro.local>
2018-06-29 13:08:48 +0800

    test

--- neo4j/main/src/java/HelloWorld.java
+++ neo4j/main/src/java/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
-  public void static main(String args[]){
-    System.out.println("Hello world!")
+  public static void main(String args[]){
+    System.out.println("Hello world!");
   }
 }
